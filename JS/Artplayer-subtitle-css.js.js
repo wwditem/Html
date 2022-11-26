ef1ca084915df5ece0b1d48bdd970cfa94a085f5
2992204600 <90314944+2992204600@users.noreply.github.com>
2022-11-26 21:11:15 +0800

    Add files via upload

---
+++ JS/Artplayer-subtitle-css.js.js
@@ -0,0 +1,10 @@
+art.on('ready', () => {
+		    art.seek = 20;
+		    setTimeout(() => {
+		        art.subtitle.style({
+		            color: 'FFFFFF',
+		            'font-size': '50px',
+					'margin-bottom': '30px',
+		        });
+		    }, 3000);
+		});
